cb53910db5ebe1ad5a84ed122d44093ab2cba518
fatherOfWine <eshil@hotmail.com>
2017-12-21 11:25:20 -0500

    SlavaTestWebApp
    Initail commit

---
+++ SlavaTestWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SlavaTestWebApp.MvcApplication" Language="C#" %>
---
+++ SlavaTestWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SlavaTestWebApp.Startup))]
+namespace SlavaTestWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
